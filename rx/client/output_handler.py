653a57f29c64a60ef5b3c66c9efecef56b75197b
Kris Chodorow <k.chodorow@gmail.com>
2023-12-24 13:20:29 -0500

    Remove rx-out

--- rx/client/output_handler.py
+++ rx/client/output_handler.py
@@ -1,7 +1,6 @@
 """Handle outputs from the server."""
 import pathlib
-import sys
-from typing import List
+from typing import Set
 
 from rx.client.worker import rsync
 from rx.proto import rx_pb2
@@ -12,21 +11,17 @@ class OutputHandler:
 
   def __init__(self, rxroot: pathlib.Path) -> None:
     self._rxroot = rxroot
-    self._current_outputs = set()
-
-  @property
-  def remote_paths(self) -> List[str]:
-    return sorted([f'{o}' for o in self._current_outputs])
+    self._current_outputs: Set[pathlib.Path] = set()
 
   def handle(self, resp: rx_pb2.ExecResponse):
-    """Shows output & error streams and creates output files."""
+    """Creates output files."""
     for pth_str in resp.output_files:
       self._current_outputs.add(pathlib.Path(pth_str))
 
   def write_outputs(self, rsync_client: rsync.RsyncClient):
     if not self._current_outputs:
       return
-    rsync_client.from_remote('rx-out', self._rxroot / pathlib.Path('rx-out'))
-    print('Created outputs:')
-    for pth in self.remote_paths:
+    rsync_client.from_remote('.', self._rxroot)
+    print('Changed:')
+    for pth in sorted(self._current_outputs):
       print(f'  {pth}')
