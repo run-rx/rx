4353eb32292673fc10ac9e47a463a6149065c119
Kris Chodorow <k.chodorow@gmail.com>
2024-02-02 10:35:19 -0500

    Separate out getting the project files

--- rx/client/worker/rsync.py
+++ rx/client/worker/rsync.py
@@ -9,7 +9,8 @@ from absl import logging
 from rx.client.configuration import config_base
 from rx.client.configuration import local
 
-def dry_run(local_cfg: local.LocalConfig):
+
+def get_manifest(local_cfg: local.LocalConfig) -> List[str]:
   dest = tempfile.mkdtemp()
   cmd = [
       local_cfg['rsync_path'],
@@ -23,12 +24,20 @@ def dry_run(local_cfg: local.LocalConfig):
       str(dest)]
   result = subprocess.run(cmd, check=True, capture_output=True)
   stdout = result.stdout.decode('utf-8')
-  print('Uploading:')
+  manifest = []
   for ln in stdout.split('\n'):
     delta = ln.split(' ')
     if len(delta) < 2 or delta[1] == './':
       continue
     filename = delta[1]
+    manifest.append(filename)
+  return manifest
+
+
+def dry_run(local_cfg: local.LocalConfig):
+  manifest = get_manifest(local_cfg)
+  print('Uploading:')
+  for filename in manifest:
     tab_count = 1 + filename.count('/')
     is_dir = False
     if filename.endswith('/'):
@@ -39,6 +48,7 @@ def dry_run(local_cfg: local.LocalConfig):
     pretty_name = f'{pathlib.Path(filename).name}{slash}'
     # TODO: handle file deletion.
     print(f'{tabs}{pretty_name}')
+
 
 class RsyncClient:
   """Rsync tools."""
