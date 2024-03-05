8bae82009b4b20fb95948964f49fc74e0320492b
Kris Chodorow <k.chodorow@gmail.com>
2024-03-05 10:14:35 -0500

    Bump version

--- rx/client/configuration/local.py
+++ rx/client/configuration/local.py
@@ -13,7 +13,7 @@ import yaml
 from rx.client.configuration import config_base
 from rx.proto import rx_pb2
 
-VERSION = '0.0.20'
+VERSION = '0.0.21'
 
 # Port the rx-daemon listens on.
 DEFAULT_DAEMON_PORT = 8478
